6d17e692ef074fd074e149d9ad6bdda6ab9aa132
harukodi <48220443+harukodi@users.noreply.github.com>
2022-03-21 20:31:25 +0100

    Update installKuber.sh

--- installKuber.sh
+++ installKuber.sh
@@ -30,6 +30,7 @@ sleep 2
 sudo rm ./kubectl
 sudo rm ./kubectl.sha256
 sudo rm -rf ./installKuber.sh
+sudo history -c
 #end of then in if
 else echo "checksum check canclled by user"
 sudo rm ./kubectl
