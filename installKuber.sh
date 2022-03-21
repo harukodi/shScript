012a17aa334083a0e2800174acc95d256dfb1a14
harukodi <48220443+harukodi@users.noreply.github.com>
2022-03-21 21:29:36 +0100

    Update installKuber.sh

--- installKuber.sh
+++ installKuber.sh
@@ -9,12 +9,16 @@ if [[ $(command -v curl) ]] && [[ $askPrompt = y ]]
 then
 sudo apt update
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+sudo install minikube-linux-amd64 /usr/local/bin/minikube
 echo "Want to check the checksum?"
 read askPromptCheckSum
 #end of then in if
 else 
 sudo apt install curl -y
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+sudo install minikube-linux-amd64 /usr/local/bin/minikube
 echo "Want to check the checksum?"
 read askPromptCheckSum
 #end of else in if
@@ -22,14 +26,16 @@ fi
 if [[ $askPromptCheckSum = y ]]
 then
 curl -LO "https://dl.k8s.io/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
+clear
 echo "$(<kubectl.sha256)  kubectl" | sha256sum --check
-sleep 2
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+sleep 4
 clear
 echo "Wait clearing files"
 sleep 2
 sudo rm ./kubectl
 sudo rm ./kubectl.sha256
+sudo rm ./minikube-linux-amd64
 sudo rm -rf ./installKuber.sh
 #end of then in if
 else echo "checksum check canclled by user"
